88189b0079d9549e8ff48964462cbb057c4d8fb7
Blythe Pollard <blytheepollard@gmail.com>
2019-09-25 21:30:26 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,9 +1,9 @@
 class Dog
-  def name=(dog_name)
-    this_dogs_name = dog_name
+  def name=(dog_name) #one instance method
+    this_dogs_name = dog_name #sets argument to equal variable
   end
   
-  def name
-    this_dogs_name
+  def name #another instance method
+    this_dogs_name #responsible for reporting/reading name
   end 
 end  
