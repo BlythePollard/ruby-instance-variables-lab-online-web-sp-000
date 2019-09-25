e0950a9e19329d81a0255adeec128ed1ed6d6151
Blythe Pollard <blytheepollard@gmail.com>
2019-09-25 21:33:41 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,13 +1,14 @@
 class Dog
-  def name=(dog_name) #one instance method
-    this_dogs_name = dog_name #sets argument to equal variable
+  def name=(dog_name) 
+    this_dogs_name = dog_name 
+    
   end
   
   def name #another instance method
-    this_dogs_name #responsible for reporting/reading name
+    this_dogs_name 
   end 
 end  
-#both methods= mechanisms to expose data from inside object to outside world--setting and getting a dog's name
+
 
 lassie = Dog.new
 lassie.name = "Lassie"
